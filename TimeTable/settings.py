b475694d086775264cb060a4f85f8e611a4b244a
Nadjib ATOUI <a_atoui@estin.dz>
2024-06-09 21:21:06 +0100

    add cors

--- TimeTable/settings.py
+++ TimeTable/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #add
+    'corsheaders',
     'rest_framework',
     'Time',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 #add new
